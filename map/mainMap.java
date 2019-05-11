27b5abc901ce976ff14f524256bcba30abeee3df
João Vitor <31751277+joaovitorx6@users.noreply.github.com>
2019-05-11 20:26:05 -0300

    Create mainMap.java

---
+++ map/mainMap.java
@@ -0,0 +1,17 @@
+public class mainMap {
+
+	public static void main(String[] args) {
+		Map map = new Map ();
+		node a[];
+		map.insertItem(122, 1);
+		
+//		a = map.Elements();
+//		int size = map.size();
+//		
+//		for (int i=0; i<size; i++){
+//			if (a[i]!=null){
+//				System.out.println(a[i].getElement());
+//			}
+//		}
+	}
+}
