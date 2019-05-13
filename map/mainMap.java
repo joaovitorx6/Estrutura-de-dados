681132cff7cdf11c801cd63d000361c2e2d22a2d
João Vitor <31751277+joaovitorx6@users.noreply.github.com>
2019-05-13 20:28:50 -0300

    Update mainMap.java

--- map/mainMap.java
+++ map/mainMap.java
@@ -1,17 +1,22 @@
+import java.util.Iterator;
 public class mainMap {
 
 	public static void main(String[] args) {
 		Map map = new Map ();
-		node a[];
-		map.insertItem(122, 1);
+		Iterator <Integer> keys;
+		int n;
 		
-//		a = map.Elements();
-//		int size = map.size();
-//		
-//		for (int i=0; i<size; i++){
-//			if (a[i]!=null){
-//				System.out.println(a[i].getElement());
-//			}
-//		}
+		node a[];
+		map.insertItem(26, 1);
+		map.insertItem(36, 3);
+//		map.insertItem(500, 3);
+//		map.insertItem(800, 4);
+		
+		try{
+			System.out.println(map.removeElement(26));
+			System.out.println(map.findElement(36));
+		} catch (invalidKeyException e){
+			System.out.println(e.getMessage());
+		}
 	}
 }
